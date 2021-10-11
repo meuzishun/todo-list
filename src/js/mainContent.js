d90d08f596eaa4760f2c93c8e4b9442b5b6bf049
Andrew Smith <meuzishun@gmail.com>
2021-10-11 11:53:23 -0400

    add webpack watch script

--- src/js/mainContent.js
+++ src/js/mainContent.js
@@ -16,41 +16,50 @@ const handleAddTaskBtnClick = function(e) {
 const changeMainContent = function() {
     mainContent.textContent = '';
 
+    // Create the container for the project
     const projectContainer = document.createElement('div');
     const projectTitleAsClassName = displayedProject.title.replace(/\s/g, '-');
     projectContainer.classList.add(`${projectTitleAsClassName}-container`);
     projectContainer.classList.add(`project-container`);
 
+    // Give the project a heading
     const projectHeading = document.createElement('h2');
     projectHeading.textContent = displayedProject.title;
     projectContainer.appendChild(projectHeading);
 
+    // Create a container for the tasks
     const tasksContainer = document.createElement('div');
     tasksContainer.classList.add('tasks-container');
     projectContainer.appendChild(tasksContainer);
 
+    // Loop through and find all the tasks in the project
     for (const task in displayedProject.tasks) {
         const currentTask = displayedProject.tasks[task];
         const taskTitle = currentTask.title.replace(/\s/g, '-');
 
+        // Create a task container
         const taskContainer = document.createElement('div');
         taskContainer.classList.add("single-task-container");
 
+        // Create a container for the checkbox, label, and optional project reminder
         const checkLabelContainer = document.createElement('div');
         checkLabelContainer.classList.add('check-label-container');
 
+        // Create the task checkbox
         const taskCheckbox = document.createElement('input');
         taskCheckbox.setAttribute('type', 'checkbox');
         taskCheckbox.classList.add('task-checkbox');
         taskCheckbox.id = `${taskTitle}-checkbox`;
         checkLabelContainer.appendChild(taskCheckbox);
 
+        // Create the task label
         const taskLabel = document.createElement('label');
         taskLabel.classList.add("task-label");
         taskLabel.setAttribute('for', `${taskTitle}-checkbox`);
         taskLabel.textContent = currentTask.title;
         checkLabelContainer.appendChild(taskLabel);
 
+        // Create the project reminder if necessary
         if (displayedProject.title === 'Today' || displayedProject.title === 'This week') {
             const projectReminder = document.createElement('p');
             projectReminder.classList.add('project-reminder');
@@ -58,11 +67,9 @@ const changeMainContent = function() {
             checkLabelContainer.appendChild(projectReminder);
         }
 
-        
-        
-
         taskContainer.appendChild(checkLabelContainer);
-        
+
+        // Check for a task description
         if (currentTask.description) {
             const descriptionContainer = document.createElement('div');
             descriptionContainer.classList.add('description-container');
@@ -88,10 +95,10 @@ const changeMainContent = function() {
         taskDueDate.classList.add('task-due-date');
         taskDueDate.textContent = currentTask.dueDate.toDateString();
         dueDateContainer.appendChild(taskDueDate);
-        
-        
-        
         taskContainer.appendChild(dueDateContainer);
+        
+        
+        
 
         tasksContainer.appendChild(taskContainer);
     }
