79f4e06c88435e1d6f8524ce23a481f4a23e6b27
Andrew Smith <meuzishun@gmail.com>
2021-10-20 19:12:30 -0400

    Create appStorage setup

--- src/js/appStorage.js
+++ src/js/appStorage.js
@@ -1,14 +1,71 @@
-const appStorage = {
-    props: {
-        selectedProject: 'inbox',
+class Task {
+    constructor(data) {
+        this.title = data.title;
+        this.description = data.description || null;
+        this.dueDate = data.dueDate || null;
+    }
+}
 
-    },
-    setSelectedProj: function(project) {
-        this.props.selectedProject = project;
-    },
-    getSelectionProj: function() {
-        return this.props.selectedProject;
-    },
+class Project {
+    constructor(title) {
+        this.title = title;
+        this.tasks = {};
+    }
+
+    getTasks() {
+        return this.tasks;
+    }
+
+    addTask(data) {
+        const task = new Task(data);
+        this.tasks[task.title] = task;
+    }
+
+    removeTask() {}
+}
+
+const appStorage = {
+    // props: {
+    //     selectedProject: 'inbox',
+
+    // },
+    // setSelectedProj: function(project) {
+    //     this.props.selectedProject = project;
+    // },
+    // getSelectionProj: function() {
+    //     return this.props.selectedProject;
+    // },
+
+    // properties: {
+    //     projects: {},
+    //     tasks: {},
+
+    // }
+
+    properties: {
+
+        projects: {
+            'inbox': {
+                title: 'inbox',
+                tasks: {},
+                getTasks: function() {},
+                addTask: function() {},
+                removeTask: function() {},
+            },
+            'today': {},
+            'this week': {},
+            'my project 1': {},
+            'my project 2': {},
+        },
+        getProjects: function() {
+            return properties.projects;
+        },
+        addProject: function(title) {
+            const project = new Project(title);
+            properties[project.title] = project;
+        },
+        removeProject: function(project) {}
+    }
 };
 
-export { appStorage };
+// export { appStorage };
