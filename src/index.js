169c568ffa99bfcd583dededb9f5726950242f8b
Andrew Smith <meuzishun@gmail.com>
2022-07-27 10:50:20 -0400

    I think this is correct

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
-
 import { appStorage } from './js/appStorage.js';
 import { markup } from './js/markup.js';
 import { navbar } from './js/navBar.js';
 import { forms } from './js/forms.js';
 import { mainContent } from './js/mainContent.js';
-// import './css/style.min.css';
+import './css/style.min.css';
 // import 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css';
 
 // localStorage.clear();
